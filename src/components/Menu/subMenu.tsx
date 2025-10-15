efb967b97f1407fbc8bc46fd48feefe7034a38b3
ziyanbb <2932796029@qq.com>
2025-10-15 17:47:21 +0800

    下拉框的展开和收起

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import { MenuContext } from './Menu'
 import { MenuItemProps } from './MenuItem'
 export interface SubMenuProps {
@@ -16,9 +16,20 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
     const classes = classNames('menu-item submenu-item', className, {
         'is-active': context.itemIndex === index,
     })
+    // 定义状态决定要不要显示下拉框
+    const [isOpen, setIsOpen] = useState(false)
+    // 定义回调函数，点击时触发
+    const handleClick = (e: React.MouseEvent) => {
+        e.preventDefault()
+        setIsOpen(!isOpen)
+    }
     // 因为subMenu下面本质上也是MenuItem，所以也采用孩子遍历
     const renderChild = () => {
-        const childrenComponent = React.Children.map(children, (child, i) => {
+        // 定义classname用来携带open-menu
+        const klass = classNames('viking-submenu', {
+            'open-menu': isOpen
+        })
+        const childrenComponent = React.Children.map(children, (child, index) => {
             const childElement = child as React.FunctionComponentElement<MenuItemProps>
             if(childElement.type.displayName === 'MenuItem'){
                 return React.cloneElement(childElement, {
@@ -29,7 +40,7 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
             }
         })
         return (
-            <ul className='viking-submenu'>
+            <ul className={klass}>
                {/* 这里出来的是一个一个的MenuItem组件 */}
                 {childrenComponent}
             </ul>
@@ -38,7 +49,7 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
     return (
         // 使得这个可以下拉的子菜单的classname拥有submenu-item类名
         <li key={index} className={classes}>
-            <div className='submenu-title'>
+            <div className='submenu-title' onClick={handleClick}>
                 {title}
             </div>
 
